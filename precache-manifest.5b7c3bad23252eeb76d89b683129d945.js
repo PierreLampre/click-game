56f84860c526e5cf9fd41236b94b68b4772e14fc
Justin Lampe <pierrelampre@gmail.com>
2019-12-23 12:47:27 -0500

    Updates

---
+++ precache-manifest.5b7c3bad23252eeb76d89b683129d945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366a2b73d3fbfa6dee64c94d30e0febb",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "457cde44f1d7d8585436",
+    "url": "/click-game/static/css/main.9b22d40f.chunk.css"
+  },
+  {
+    "revision": "0bd08f80a9a2245efe7b",
+    "url": "/click-game/static/js/2.030e8f3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/click-game/static/js/2.030e8f3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "457cde44f1d7d8585436",
+    "url": "/click-game/static/js/main.5e81f464.chunk.js"
+  },
+  {
+    "revision": "64cffc606683d1ab19da",
+    "url": "/click-game/static/js/runtime-main.2c4747e0.js"
+  }
+]);
